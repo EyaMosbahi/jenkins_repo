fc49e4b16b564bc0dba8183e4fe4226ed1bcf741
EyaMosbahi <128031131+EyaMosbahi@users.noreply.github.com>
2025-12-02 16:24:21 +0100

    Change StudentControllerTest to a nested class

--- src/test/java/tn/esprit/studentmanagement/controller/StudentControllerTest.java
+++ src/test/java/tn/esprit/studentmanagement/controller/StudentControllerTest.java
@@ -11,7 +11,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @ActiveProfiles("test")
-public class StudentControllerTest {
+    class StudentControllerTest {
 
     @LocalServerPort
     private int port;
